4bcb36e906171db791d04d6d569a225310e3d510
Emilio <blim_27@outlook.es>
2023-05-02 10:25:13 +0200

    class-apr-2023

--- Ejercicios en general/labs-IronHack/js/lab-js-basic-algorithms-es-master/js/index.js
+++ Ejercicios en general/labs-IronHack/js/lab-js-basic-algorithms-es-master/js/index.js
@@ -53,7 +53,72 @@ hackerOne.length > hackerTwo.length ? console.log(`HackerOne tiene mas caractere
 // gracias a los frameworks como Vuejs , react y angular
 
 
-// Iteration 3: Loops
+// ITERACION 3.1
 
 
+ let conductor0 = "John";
+ let navegador0 = "";
 
+ for (let index = 0; index < conductor0.length; index++) {
+   
+ navegador0 += conductor0[index].toUpperCase() + " ";
+   
+ }
+ console.log(navegador0);
+
+
+// ITERACION 3.2
+
+
+ let nombre_conductor = "John"
+ let nombre_navegador = "John"
+let nombre_navegador_reverso = "";
+for (let i = nombre_navegador.length - 1; i >= 0; i--) {
+nombre_navegador_reverso += nombre_navegador[i];
+ }
+console.log(nombre_navegador_reverso);
+
+
+// ITERACION 3.3
+
+// EL NOMBRE DEL CONDUCTOR VA PRIMERO 
+
+ let conductor1= ""
+ let navegador1 = "John"
+
+if (conductor1 < navegador1) {
+console.log("El nombre del conductor va primero.");
+ } else if (conductor1 > navegador1) {
+console.log("Yo, el navegador va primero definitivamente.");
+} else {
+ console.log("¿Qué? ¿Los dos tienen el mismo nombre?");
+
+}
+
+// YO, EL NAVEGADOR VA PRIMERO DEFINITIVAMENTE
+
+let name_conductor = "John"
+ let name_navegador = ""
+
+if (name_conductor < name_navegador) {
+   console.log("El nombre del conductor va primero.");
+} else if (name_conductor > name_navegador) {
+console.log("Yo, el navegador va primero definitivamente.");
+} else {
+console.log("¿Qué? ¿Los dos tienen el mismo nombre?");
+
+}
+
+
+// ¿QUE? ¿LOS DOS TIENEN EL MISMO NOMBRE?
+
+let conductor = "John"
+ let navegador = "John"
+
+if (conductor < navegador) {
+   console.log("El nombre del conductor va primero.");
+} else if (conductor > navegador) {
+console.log("Yo, el navegador va primero definitivamente.");
+} else {
+console.log("¿Qué? ¿Los dos tienen el mismo nombre?");
+}
