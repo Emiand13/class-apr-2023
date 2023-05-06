2bc185a2280b0fe9f646361bbb957353dfedf83f
Emilio <blim_27@outlook.es>
2023-05-06 10:43:40 +0200

    class-apr-2023

--- Ejercicios en general/IronEmilio/tarjeta-3d-flip/pruebas/index.js
+++ Ejercicios en general/IronEmilio/tarjeta-3d-flip/pruebas/index.js
@@ -43,3 +43,188 @@ console.log(`La palabra "et" aparece ${contador} veces en el texto.`);
 
 
 
+// // function declaration (statement)
+// function welcomeMessage(message) {
+//   return message;
+// }
+
+// // function assigned to a variable
+// const greeting = welcomeMessage("Muy buenas estamos aprendiendo js");
+
+// console.log(greeting); // So nice to have you here! Welcome!
+
+
+
+// function expression - is a function without name stored in a variable
+const welcomeMessage = function (message) {
+  console.log(message);
+};
+
+welcomeMessage("So nice to have you here! Welcome!");
+// => So nice to have you here! Welcome!
+
+
+
+
+
+
+// let addition = 0;
+
+// let number = parseInt(prompt("Escribe un numero:"));
+
+// while (number >= 0){
+
+// addition += number;
+// number = parseInt(prompt ("Escribe un numero:"));
+
+// }
+
+// console.log(`la suma total de estos dos números usando un ciclo while es: $(addition) `);
+
+
+
+// // function declaration (statement)
+
+// checkFuncDeclaration(); // => Func declaration CAN be invoked before it's defined.
+
+// function checkFuncDeclaration() {
+//   console.log("Func declaration CAN be invoked before it is defined.");
+// }
+
+// function expression
+
+// checkFuncExpression(); // => ReferenceError: checkFuncExpression is not defined
+
+// const checkFuncExpression = function () {
+//   console.log("Func expression CAN NOT be invoked before it is defined.");
+// };
+
+
+
+// const language = prompt(
+//   "Please enter the language - (options: Spanish, French, English): "
+// );
+// const name = prompt("Please enter your name: ");
+ 
+// if (language === "Spanish") {
+//   console.log(`Hola, ${name}`);
+// } else if (language === "French") {
+//   console.log(`Salut, ${name}`);
+// } else if (language === "English") {
+//   console.log(`Hello, ${name}`);
+// } else {
+//   console.log("I am not sure which language that is! Sorry!");
+// }
+
+
+// const language = prompt("Please enter the language - (options: Spanish, French, English): ");
+// const name = prompt("Please enter your name: ");
+ 
+// switch (language) {
+//   case "Spanish":
+//     console.log(`Hola, ${name}!`);
+//     break;
+//   case "French":
+//     console.log(`Salut, ${name}!`);
+//     break;
+//   case "English":
+//     console.log(`Hello, ${name}!`);
+//     break;
+//   default:
+//     console.log("I am not sure which language that is! Sorry!");
+// }
+
+
+
+
+if ("false") {
+  console.log("Passed thingy is truthy");
+} else {
+  console.log("Passed thingy is falsy");
+}
+
+// => 'Passed thingy is truthy'
+
+
+
+// const message = "Don't be sad, be happy!";
+// console.log(message.slice(0, 3)); // <== Don
+// console.log(message); // <== Don't be sad, be happy!
+
+
+
+
+
+// // Game of Thrones Switch
+
+// const name = prompt("Your favorite Game of Thrones character?");
+ 
+// let house = "";
+// switch (name) {
+//   case "Daenerys":
+//     house = "Targaryen";
+//     break;
+//   case "Jon Snow":
+//   case "Sansa":
+//   case "Arya":
+//     house = "Stark";
+//     break;
+//   case "Tyrion":
+//   case "Cersei":
+//     house = "Lannister";
+//     break;
+// }
+ 
+// console.log(`Your favorite character is from the house ${house}.`);
+
+
+// BUCLES E ITERACCIONES 1
+
+// Iteración 1 - Crea un bucle while que cuente desde 0 hasta 30
+
+
+ 
+let i = 0;
+while (i <= 30) {
+  console.log(i);
+  i++;
+}
+  // Iteración 2 - Cambia el script anterior para escribir “diez” cuando el valor del contador sea 10, y “veinte” cuando el valor del contador sea 20. Resultado esperado:
+
+
+  // iteration 2
+ 
+// let i = 0;
+// while (i < 30) {
+//   i++; // this is the same as i = i + 1
+//   if (i === 10) {
+//     console.log("ten");
+//   } else if (i === 20) {
+//     console.log("twenty");
+//   } else if (i === 30) {
+//     console.log("thirty");
+//   } else {
+//     console.log(i);
+//   }
+//   }
+
+
+
+// Bucles e Iteraciones #2
+// Instrucciones
+// Crea un bucle for que cuente del 0 al 20. En cada iteración, comprueba si el número actual es par o impar, y escríbelo en la consola.
+
+// Hint: Necesitarás el operador de remainder (resto) para esto.
+
+
+// Resultado esperado:
+
+// 1 is odd 2 is even … 19 is odd 20 is even
+
+for (let i = 0; i <= 20; i++) {
+  if (i % 2 === 0) {
+    console.log(i + " is even");
+  } else {
+    console.log(i + " is odd");
+  }
+}
