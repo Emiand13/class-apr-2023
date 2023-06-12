3769f0016f1475f240cbf52cd990ba7d834122f7
Emilio <blim_27@outlook.es>
2023-06-12 12:38:08 +0200

    cfus-semana-9

---
+++ OneDrive/Escritorio/class-apr-2023-main/cfus-semana-9-vue.js/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Card from './components/Card.vue'
+let title = 'Este es el titulo'
+</script>
+<template>
+  <Card >
+    <template #header>
+      <h1>{{title}}</h1>
+    </template>
+    <template #default>
+      <p>Este el contenido</p>
+    </template>
+  </Card>
+
+</template>
+
+<style>
+:root {
+  height: 100vh;
+  display: grid;
+  place-items: center;
+}
+</style>
